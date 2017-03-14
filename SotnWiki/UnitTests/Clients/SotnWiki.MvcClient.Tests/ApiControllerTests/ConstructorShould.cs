fd0ff1a9499df16989fec59e248cc898dfe21283
TalicZealot <talic.zealot@gmail.com>
2017-03-14 09:42:44 +0200

    By following the freezing moon / availability tests

---
+++ SotnWiki/UnitTests/Clients/SotnWiki.MvcClient.Tests/ApiControllerTests/ConstructorShould.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using NUnit.Framework;
+using SotnWiki.DataServices.Contracts;
+using SotnWiki.MvcClient.Controllers;
+using System;
+
+namespace SotnWiki.MvcClient.Tests.ApiControllerTests
+{
+    public class ConstructorShould
+    {
+        [Test]
+        public void ThrowArgumentNullExceptionIfPageServiceIsNull()
+        {
+            //Arrange
+            var mockedRunService = new Mock<IRunService>();
+            string expectedExceptionMessage = "IPageService";
+
+            //Act
+            var exc = Assert.Throws<ArgumentNullException>(() => { new ApiController(null, mockedRunService.Object); });
+
+            //Assert
+            StringAssert.Contains(expectedExceptionMessage, exc.Message);
+        }
+
+        [Test]
+        public void ThrowArgumentNullExceptionIfRunServiceIsNull()
+        {
+            //Arrange
+            var mockedPageService = new Mock<IPageService>();
+            string expectedExceptionMessage = "IRunService";
+
+            //Act
+            var exc = Assert.Throws<ArgumentNullException>(() => { new ApiController(mockedPageService.Object, null); });
+
+            //Assert
+            StringAssert.Contains(expectedExceptionMessage, exc.Message);
+        }
+    }
+}
