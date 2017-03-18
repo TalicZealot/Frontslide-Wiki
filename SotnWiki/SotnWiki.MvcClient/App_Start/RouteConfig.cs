ce20a19ab2d6ccbc528aa337cf3da8c623adfbf2
TalicZealot <talic.zealot@gmail.com>
2017-03-18 16:36:36 +0200

    the saga of illogical OAuth problems continues

--- SotnWiki/SotnWiki.MvcClient/App_Start/RouteConfig.cs
+++ SotnWiki/SotnWiki.MvcClient/App_Start/RouteConfig.cs
@@ -53,8 +53,8 @@ namespace SotnWiki.MvcClient
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}",
-                defaults: new { controller = "Home", action = "Index"}
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
